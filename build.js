ee8e224a0087052d008c95116708281b1ece5858
Jarred Sumner <jarred@jarredsumner.com>
2021-02-14 01:23:49 -0800

    no need to minify, move to root

--- build.js
+++ build.js
@@ -4,8 +4,8 @@ build({
   bundle: true,
   target: ["node12"],
   entryPoints: ["./src/index.ts"],
-  outfile: "./lib/index.js",
+  outfile: "./index.js",
   platform: "node",
   external: ["path", "fs", "child_process", "register-url-win64-bin", "which"],
-  minify: true,
+  minify: false,
 }).then((a) => console.log("Built.", a.warnings, a.outputFiles));
